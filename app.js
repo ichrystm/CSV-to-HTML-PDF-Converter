9e988890f2b6dd0eb3f442ac9117638d0e2345fe
ichrystm <ichrystm@gmail.com>
2020-12-09 15:46:04 -0300

    NodeConversor

---
+++ app.js
@@ -0,0 +1,22 @@
+var Reader = require("./Reader");
+var Processor = require("./Processor");
+var Table = require("./table");
+var HtmlParser = require("./HtmlParser");
+var Writer = require("./Writer");
+
+var leitor = new Reader();
+var escritor = new Writer();
+
+
+async function main(){
+  var dados = await leitor.Read("./arquivo.csv");
+  var dadosProcessados = Processor.Process(dados);
+  var usuarios = new Table(dadosProcessados);
+
+  var html = await HtmlParser.Parse(usuarios);
+  
+  escritor.Write(Date.now() + ".html", html);
+
+}
+
+main();
